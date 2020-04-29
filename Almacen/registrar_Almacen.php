30e45609033ddf2b54ff37a5987460f212094fc1
vmah <vavila1@me.com>
2020-04-29 09:23:08 -0500

    Arreglando header de insertar almacen

--- Almacen/registrar_Almacen.php
+++ Almacen/registrar_Almacen.php
@@ -3,12 +3,12 @@
     session_start();
 
     require_once("model_almacen.php");
+    include("partials/despliegue_registrarAlmacen.php");
 
     include("partials/_header.html");
     include("partials/_nav.html");
     include("partials/_registraralmacen.html");
     include("partials/_formregistrarAlmacen.html");
-    include("partials/despliegue_registrarAlmacen.php");
     include("partials/_almacenes2.html");
     include("partials/_footer.html");
 
