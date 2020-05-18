7d1011257b111ed65b94252ee2c312b1ca4c59b8
Jorge López Chávez <jorge.lopez@ciasagroup.com.mx>
2020-05-17 20:52:42 -0500

    Editar Producto

--- Almacen/controlador_editar_producto.php
+++ Almacen/controlador_editar_producto.php
@@ -5,9 +5,26 @@
     //Traemos libreria de model
     require_once("model_producto.php");
 
-    include("partials/_header.html");
-    include("partials/_nav.html");
-    include("partials/_form_editar_producto.html");
-    include("partials/_footer.html");
+        if (isset($_POST["nombre"]) && isset($_POST["estatus_producto"]) && isset($_POST["precio"]) && isset($_POST["tipo_producto"]) && isset($_POST["marca"])) {
+        //var_dump($_POST["descripcion"]);
+        $nombre = htmlspecialchars($_POST["nombre"]);
+        $id_estatus = htmlspecialchars($_POST["estatus_producto"]);
+        $precio = htmlspecialchars($_POST["precio"]);
+        $id_tipo = htmlspecialchars($_POST["tipo_producto"]);
+        $id_marca = htmlspecialchars($_POST["marca"]);
+        
+
+        editar_producto($nombre, $precio, $id_marca, $id_tipo,$id_estatus,$_GET['id']);
+        $id_historial = consultar_ultimo_estado($_GET['id']);
+        editar_producto_estatus($id_historial, $id_estatus);
+        $_SESSION["mensaje"] = "Se completo el registro";
+        }
+
+    else {
+            $_SESSION["warning"] = "Ocurrio un error al registar el producto";
+        }
+    
+
+    header("location:productos.php");
 
 ?>
