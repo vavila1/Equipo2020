dfe18be5f8507d791fd8f168ad6926034ae3f264
Jorge Andres <A01209043@itesm.mx>
2020-06-10 23:38:50 -0500

    RBAC

--- Almacen/configuracion.php
+++ Almacen/configuracion.php
@@ -3,11 +3,13 @@
 
     require_once("model_producto.php");
 
-     
+     if ($_SESSION["VerMarcas"]) { 
         include("partials/_header.html");
         include("partials/_nav.html");
         include("partials/_configuracion.html");
         include("partials/_footer.html");
-    
+    } else {
+    	header("location:logout.php");
+    }
 
 ?>
