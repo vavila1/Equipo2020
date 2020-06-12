68840eb27ec01ef09fa84a98aa2cb97582388c8a
vmah <vavila1@me.com>
2020-06-12 10:25:26 -0500

    Subiendo model reportes arreglado

--- Almacen/model_reportes.php
+++ Almacen/model_reportes.php
@@ -26,9 +26,9 @@
 		//Primero conectarse a la bd
 		$conexion_bd = conectar_bd();
 
-		$resultado = "<table class=\"highlight\"><thead><tr><th>ID Producto</th><th>Producto</th><th>Tipo de transaccion</th><th>Empleado Responsable</th><th>Cantidad</th><th>Precio unitario</th><th>Total</th><th>Fecha</th><th>Proyecto</th></tr></thead>";
+		$resultado = "<table class=\"highlight\"><thead><tr><th>ID Producto</th><th>Producto</th><th>Empleado Responsable</th><th>Tipo de transaccion</th><th>Supervisor</th><th>Cantidad</th><th>Precio unitario</th><th>Total</th><th>Fecha</th><th>Proyecto</th></tr></thead>";
 
-		$consulta = 'SELECT p.Id_Almacen as p_Id_Almacen, p.precio as p_precio, e.cantidad as e_cantidad, p.id as p_id, t.Nombre as t_nombre, p.nombre as p_nombre, emp.Nombre as emp_nombre, e.fecha as e_fecha,e.proyecto as e_proyecto, SUM(p.precio * e.cantidad) as total
+		$consulta = 'SELECT e.supervisor as e_supervisor, p.Id_Almacen as p_Id_Almacen, p.precio as p_precio, e.cantidad as e_cantidad, p.id as p_id, t.Nombre as t_nombre, p.nombre as p_nombre, emp.Nombre as emp_nombre, e.fecha as e_fecha,e.proyecto as e_proyecto, SUM(p.precio * e.cantidad) as total
 						FROM entregan as e, producto as p, empleado as emp, transaccion as t, almacen as alm 
 						WHERE e.Id_Transaccion = t.Id_Transaccion AND e.Id_Producto = p.id AND e.Id_Empleado = emp.Id_Empleado AND p.Id_Almacen=alm.id'; 
 		if($fecha_inicio!="" && $fecha_fin!=""){
@@ -61,8 +61,9 @@
 		    $resultado .= "<td>".$row['p_id']."</td>";
 		    $resultado .= "<td>".$row['p_nombre']."</td>";
 		    
-		    $resultado .= "<td>".$row['t_nombre']."</td>";
 		    $resultado .= "<td>".$row['emp_nombre']."</td>";
+		    $resultado .= "<td>".$row['t_nombre']."</td>";
+		    $resultado .= "<td>".$row['e_supervisor']."</td>";
 		    $resultado .= "<td>".$row['e_cantidad']."</td>";
 		    $resultado .= "<td>$".$row['p_precio']."</td>";
 		    $resultado .= "<td> $ ".$row['total']."</td>";
@@ -72,6 +73,7 @@
 		}else{
 			$resultado .= "<td>".$row['e_proyecto']."</td>";
 		}
+		    
 			
 		    $resultado .= "</tr>" ;
 		}
